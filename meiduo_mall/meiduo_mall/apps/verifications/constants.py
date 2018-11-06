c31939a44723ac795dd89cfbe2ff6b2c1a1fe861
xwp <xwp_fullstack@163.com>
2018-11-06 15:32:34 +0800

    项目搭建及配置&图片验证码的前后端实现

---
+++ meiduo_mall/meiduo_mall/apps/verifications/constants.py
@@ -0,0 +1,2 @@
+# 图片验证码的有效期， 单位是秒
+IMAGE_CODE_REDIS_EXPIRES = 300
---
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django_redis import get_redis_connection
+from django.http.response import HttpResponse
+from rest_framework.views import APIView
+
+from meiduo_mall.libs.captcha.captcha import captcha
+from . import constants
+
+# Create your views here.
+
+
+class ImageCodeView(APIView):
+    """
+    图片验证码
+    """
+
+    def get(self, request, image_code_id):
+        """
+        获取图片验证码
+        """
+        # 生成验证码图片
+        text, image = captcha.generate_captcha()
+
+        redis_conn = get_redis_connection("verify_codes")
+        redis_conn.setex("img_%s" % image_code_id, constants.IMAGE_CODE_REDIS_EXPIRES, text)
+
+        # 固定返回验证码图片数据，不需要REST framework框架的Response帮助我们决定返回响应数据的格式
+        # 所以此处直接使用Django原生的HttpResponse即可
+        return HttpResponse(image, content_type="images/jpg")
+
