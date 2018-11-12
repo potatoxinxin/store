de62a51f269abfe22053df87e23841c24e673dfa
QYF <qinyifan__123@163.com>
2018-11-12 21:46:05 +0800

    省市区视图集的前后端编写

--- meiduo_mall/meiduo_mall/apps/areas/views.py
+++ meiduo_mall/meiduo_mall/apps/areas/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from .models import Area
+from . import serializers
 
 # Create your views here.
+
+
+class AreasViewSet(ReadOnlyModelViewSet):
+    """
+    list:
+    返回所有省份的信息
+
+    retrieve:
+    返回特定省或市的下属行政规划区域
+    """
+    # queryset = Area.objects.all()
+
+    def get_queryset(self):
+        """
+        提供数据集
+        """
+        if self.action == 'list':
+            return Area.objects.filter(parent=None)
+        else:
+            return Area.objects.all()
+
+    def get_serializer_class(self):
+        """
+        提供序列化器
+        """
+        if self.action == 'list':
+            return serializers.AreaSerializer
+        else:
+            return serializers.SubAreaSerializer
+
+
+
